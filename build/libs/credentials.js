0c23d02b5223e9c68d57e16ccc0ff2b1d5825ca1
gabriel-acuna <gabriel.acuna.reg27@gmail.com>
2023-10-04 23:40:52 -0500

    Setting repo

---
+++ build/libs/credentials.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getPCK12Cert = void 0;
+function getPCK12Cert() { }
+exports.getPCK12Cert = getPCK12Cert;
---
+++ src/main.ts
@@ -0,0 +1,1 @@
+console.log('XAdES-BES')
