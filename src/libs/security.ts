bdb769822bc6ed17e071bb461b9554adf3957271
gabriel-acuna <gabriel.acuna.reg27@gmail.com>
2023-10-18 23:18:19 -0500

    Add some functions in security module

--- src/libs/security.ts
+++ src/libs/security.ts
@@ -4,3 +4,27 @@ export function sha1ToBase64(text: string): string {
     const BUFFER = Buffer.from(HASH, 'hex')
     return BUFFER.toString('base64')
 }
+
+export function hexToBase64(hashHex: string){
+    hashHex.padStart(hashHex.length +
+        (hashHex.length %2), '0');
+    const bytes = hashHex.match(/.{2}/g)?.map(
+        byte => parseInt(byte, 16)
+    );
+   return bytes ? Buffer.from(String.fromCharCode( ...bytes), 'base64')
+   .toString('base64') : '';
+}
+
+export function bigintToBase64(param: bigint){
+    const hexString = param.toString(16);
+    const hexPairs = hexString.match(/\w{2}/g);
+    const bytes = hexPairs?.map( (pair) => parseInt(pair, 16));
+    const byteString = bytes ? String.fromCharCode(...bytes) : ''
+    const base64 = btoa(byteString);
+    const formatedBase64 = base64.match(/.{1,76}/g)?.join('\n');
+    return formatedBase64;
+}
+
+export function getRandomValues(min: number = 990, max: number = 9999){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
